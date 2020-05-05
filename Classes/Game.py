ffa0b73e993fee4fceb1ea7fdcbb22c4561fd6b1
Amit <amit@chorus.ai>
2020-05-05 11:52:23 +0300

    refactor

--- Classes/Game.py
+++ Classes/Game.py
@@ -9,37 +9,40 @@ class Game:
         self.screen = pygame.display.set_mode([800, 800])
         self.size = 16
         pygame.display.set_caption('Mazal Tov!')
+        self.crossword = Crossword(self.screen, self.size)
 
     def start_game(self):
         self.screen.fill((255, 255, 255))
-        crossword = Crossword(self.screen, self.size)
-        crossword.init_grid()
+        self.crossword.init_grid()
         self.running = True
         while self.running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
                 if event.type == pygame.KEYDOWN:
-                    selected = crossword.get_selected_cell()
-                    if event.key == 273:
-                        new_cell = crossword.find_cell(max(0, selected.row-1),
-                                                       selected.col)
-                        crossword.set_selected_cell(new_cell)
-                    elif event.key == 274:
-                        new_cell = crossword.find_cell(min(self.size-1, selected.row+1),
-                                                       selected.col)
-                        crossword.set_selected_cell(new_cell)
-                    elif event.key == 275:
-                        new_cell = crossword.find_cell(selected.row,
-                                                       min(self.size - 1, selected.col + 1))
-                        crossword.set_selected_cell(new_cell)
-                    elif event.key == 276:
-                        new_cell = crossword.find_cell(selected.row,
-                                                       max(0, selected.col - 1))
-                        crossword.set_selected_cell(new_cell)
+                    self.move_around(event)
 
-            crossword.draw_graph()
+            self.crossword.draw_graph()
 
             pygame.display.flip()
 
         pygame.quit()
+
+    def move_around(self, event):
+        selected = self.crossword.get_selected_cell()
+        if event.key == 273:
+            new_cell = self.crossword.find_cell(max(0, selected.row - 1),
+                                           selected.col)
+            self.crossword.set_selected_cell(new_cell)
+        elif event.key == 274:
+            new_cell = self.crossword.find_cell(min(self.size - 1, selected.row + 1),
+                                           selected.col)
+            self.crossword.set_selected_cell(new_cell)
+        elif event.key == 275:
+            new_cell = self.crossword.find_cell(selected.row,
+                                           min(self.size - 1, selected.col + 1))
+            self.crossword.set_selected_cell(new_cell)
+        elif event.key == 276:
+            new_cell = self.crossword.find_cell(selected.row,
+                                           max(0, selected.col - 1))
+            self.crossword.set_selected_cell(new_cell)
